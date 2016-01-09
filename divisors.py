3239ddf6d6867bbfad1ee7dc6c2b5cadcbdf1594
noga17-meet <noga17@meet.mit.edu>
2016-01-09 12:30:01 +0200

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -5,3 +5,14 @@ def divisors (m):
 		if m % i==0:
 			print(i)
 divisors(10)
+
+
+#EXERCISE B
+m= input()
+for i in m:
+    if m[0]==m[-1]:
+        print ("TRUE")
+    #elif m[1]==m[-2]: (cant be because if there is word with 3 letters but it doesnt pakindrome)
+        #print ("TRUE")
+    else :
+        print ("FALSE")
