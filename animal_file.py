9a317cf545f6efc3bd3fad5999dc7b1da19c9bff
brit13-meet <brit13@meet.mit.edu>
2016-01-13 19:59:36 +0200

    oop

---
+++ animal_file.py
@@ -0,0 +1,7 @@
+class Animal (object):
+	def __init__(self,name,color,age):
+		self.color= color
+		self.name= name
+		self.age= age
+	def Noga(self):
+		print(self.name, "is" ,self.age, "years old and his color is", self.color)
